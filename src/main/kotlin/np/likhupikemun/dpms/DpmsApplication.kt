f4cc03faabcd0e2de485684e57c5417e4b60d980
= <=>
2025-03-12 02:25:09 -0700

    feat: bump to 0.0.1-snapshot

---
+++ src/main/kotlin/np/likhupikemun/dpms/DpmsApplication.kt
@@ -0,0 +1,11 @@
+package np.likhupikemun.dpms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DpmsApplication
+
+fun main(args: Array<String>) {
+	runApplication<DpmsApplication>(*args)
+}
---
+++ src/test/kotlin/np/likhupikemun/dpms/DpmsApplicationTests.kt
@@ -0,0 +1,13 @@
+package np.likhupikemun.dpms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DpmsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
