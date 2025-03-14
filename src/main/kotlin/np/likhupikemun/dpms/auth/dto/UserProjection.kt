f58d4b1c36095ff11384caafcba1fadab7e8c8ac
fernsky <trilochanbhusal000@gmail.com>
2025-03-14 07:50:49 +0545

    feat: create user projection utilities

---
+++ src/main/kotlin/np/likhupikemun/dpms/auth/dto/UserProjection.kt
@@ -0,0 +1,29 @@
+package np.likhupikemun.dpms.auth.dto
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import np.likhupikemun.dpms.auth.domain.enums.PermissionType
+import java.time.LocalDateTime
+import java.util.UUID
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+interface UserProjection {
+    fun getId(): UUID?
+
+    fun getEmail(): String?
+
+    fun getPermissions(): Set<PermissionType>?
+
+    fun getIsWardLevelUser(): Boolean?
+
+    fun getWardNumber(): Int?
+
+    fun getIsApproved(): Boolean?
+
+    fun getApprovedBy(): UUID?
+
+    fun getApprovedAt(): LocalDateTime?
+
+    fun getCreatedAt(): LocalDateTime?
+
+    fun getUpdatedAt(): LocalDateTime?
+}
