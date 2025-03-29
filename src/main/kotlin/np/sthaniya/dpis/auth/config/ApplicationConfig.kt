ccc64b8b4924b5939d6f003f93f8942fb84d3ed4
fernsky <trilochanbhusal000@gmail.com>
2025-03-29 08:01:49 +0545

    bug: fix import issue in ApplicationConfig

--- src/main/kotlin/np/sthaniya/dpis/auth/config/ApplicationConfig.kt
+++ src/main/kotlin/np/sthaniya/dpis/auth/config/ApplicationConfig.kt
@@ -1,5 +1,16 @@
 package np.sthaniya.dpis.auth.config
 
+import np.sthaniya.dpis.auth.repository.UserRepository
+import np.sthaniya.dpis.auth.exception.AuthException
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.password.PasswordEncoder
+
 /**
  * Spring Security configuration class that sets up authentication-related beans.
  *
