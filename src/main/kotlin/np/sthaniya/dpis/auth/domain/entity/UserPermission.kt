d228f6eb5f3dad18ac19991aa8b280327ff7787f
fernsky <trilochanbhusal000@gmail.com>
2025-03-29 07:28:29 +0545

    docs: enhance UserPermission documentation with detailed descriptions and usage examples

--- src/main/kotlin/np/sthaniya/dpis/auth/domain/entity/UserPermission.kt
+++ src/main/kotlin/np/sthaniya/dpis/auth/domain/entity/UserPermission.kt
@@ -3,6 +3,28 @@ package np.sthaniya.dpis.auth.domain.entity
 import jakarta.persistence.*
 import np.sthaniya.dpis.common.entity.UuidBaseEntity
 
+/**
+ * Represents the many-to-many relationship between users and permissions in the system.
+ *
+ * This entity serves as a join table between [User] and [Permission] entities,
+ * maintaining the association between users and their assigned permissions.
+ * It includes additional auditing capabilities inherited from [UuidBaseEntity].
+ *
+ * Features:
+ * - Unique constraint on user-permission combination
+ * - Lazy loading of relationships for optimal performance
+ * - Inherits UUID-based identification and auditing from [UuidBaseEntity]
+ *
+ * Usage:
+ * ```kotlin
+ * val userPermission = UserPermission.create(user, permission)
+ * // or
+ * val userPermission = UserPermission(user, permission)
+ * ```
+ *
+ * @property user The user to whom the permission is assigned
+ * @property permission The permission being assigned to the user
+ */
 @Entity
 @Table(
     name = "user_permissions",
@@ -22,7 +44,15 @@ class UserPermission(
     @JoinColumn(name = "permission_type", nullable = false, referencedColumnName = "type")
     var permission: Permission
 ) : UuidBaseEntity() {
+    
     companion object {
+        /**
+         * Factory method to create a new UserPermission instance.
+         * 
+         * @param user The user to whom the permission will be assigned
+         * @param permission The permission to be assigned
+         * @return A new UserPermission instance
+         */
         fun create(user: User, permission: Permission) = UserPermission(user, permission)
     }
 }
