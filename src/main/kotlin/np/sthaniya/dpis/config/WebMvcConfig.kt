584feba4b86855f65d5a27f8730c7583c33ae6fe
fernsky <trilochanbhusal000@gmail.com>
2025-04-07 09:39:22 +0545

    feat: Update WebMvcConfig to include CurrentCitizenIdResolver for JWT-based citizen ID resolution

--- src/main/kotlin/np/sthaniya/dpis/config/WebMvcConfig.kt
+++ src/main/kotlin/np/sthaniya/dpis/config/WebMvcConfig.kt
@@ -2,6 +2,7 @@ package np.sthaniya.dpis.config
 
 import np.sthaniya.dpis.auth.resolver.CurrentUserIdResolver
 import org.springframework.context.annotation.Configuration
+import np.sthaniya.dpis.citizen.resolver.CurrentCitizenIdResolver
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -10,19 +11,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
  *
  * This configuration provides custom argument resolvers for:
  * - Current user ID resolution in controller methods
+ * - Current citizen ID resolution in controller methods
  * - Integration with Spring MVC's method parameter resolution
  */
 @Configuration
-class WebMvcConfig : WebMvcConfigurer {
+class WebMvcConfig(
+    private val currentUserIdResolver: CurrentUserIdResolver,
+    private val currentCitizenIdResolver: CurrentCitizenIdResolver
+) : WebMvcConfigurer {    
+
     /**
      * Adds custom argument resolvers to Spring MVC's parameter resolution chain.
      *
      * Registers:
      * - [CurrentUserIdResolver] for resolving current user IDs from security context
+     * - [CurrentCitizenIdResolver] for resolving current citizen IDs from JWT tokens
      *
      * @param resolvers The list of argument resolvers to extend
      */
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
-        resolvers.add(CurrentUserIdResolver())
+        resolvers.add(currentUserIdResolver)
+        resolvers.add(currentCitizenIdResolver)
     }
 }
